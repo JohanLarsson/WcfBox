5f22f393e49ae8fbd85b58ab853da1d98c10cd1a
Johan Larsson <johan_gavle1@hotmail.com>
2014-03-01 19:31:41 +0100

    Message

---
+++ Contracts/DummyMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts
+{
+    using System;
+
+    public class DummyMessage
+    {
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+    }
+}
